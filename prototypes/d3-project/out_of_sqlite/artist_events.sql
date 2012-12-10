da504c82ea67866d88b476946c5c4f1045773ac2
Rocky Fischer <adorack@eventide.local>
2012-12-10 12:39:53 -0500

    Updated SQL for the Postgres tables.

--- prototypes/d3-project/out_of_sqlite/artist_events.sql
+++ prototypes/d3-project/out_of_sqlite/artist_events.sql
@@ -1,4 +1,12 @@
 CREATE TABLE artist_events (
-	event_id integer,
-	artist_name character varying(838)
+	event_id integer not null,
+	artist_name character varying(838) not null,
 );
+
+-- CONSTRAINT artist_events_pkey PRIMARY KEY (event_id)
+
+CREATE INDEX artist_events_artist_index
+   ON artist_events (artist_name ASC);
+
+CREATE INDEX artist_events_event_index
+   ON artist_events (event_id ASC);
