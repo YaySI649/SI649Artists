98d8ca719ed41cfe37fb12f96ba1fbda934af412
richelite <richeliteys@gmail.com>
2012-12-16 15:31:23 -0500

    add highlight bar function

--- prototypes/artist/artist.js
+++ prototypes/artist/artist.js
@@ -96,13 +96,14 @@ $(document).ready(function(){
 		svg.selectAll(".bar")
 		      .data(data)
 		    .enter().append("rect")
+		      .attr("id", function(d){ return d.year;})
 		      .attr("class", "bar")
 		      .attr("x", function(d) { return x(d.year); })
 		      .attr("width", x.rangeBand())
 		      .attr("y", function(d) { return y(d.events); })
 		      .attr("height", function(d) { return height - y(d.events); })
 		      .attr('stroke', 'white')
-			  .attr('fill', 'steelblue');
+			  .attr('fill', 'grey');
 
 		svg.selectAll(".label")
 			  .data(data)
@@ -138,7 +139,8 @@ $(document).ready(function(){
                 document.getElementById('min_year').innerHTML = lower_year;
                 document.getElementById('max_year').innerHTML = higher_year;
                 //redraw visualization by new filter value
-                console.log(lower_year,higher_year);
+                highlightBar(lower_year, higher_year);
+                //console.log(lower_year,higher_year);
                 //drawVisualization(lower_finance/100,higher_finance/100,lower_poverty/100,higher_poverty/100);
             }                
         });            
@@ -146,5 +148,15 @@ $(document).ready(function(){
     };
 
     setupSlider(1960,2012);
+
+    function highlightBar(start, end){
+    	//set fill to default before highlight bars
+    	$('.bar').css("fill", "grey");
+		for(var id=start;id<=end;id++){
+			var year = '#' + id;
+			$(year).css("fill", "steelblue");
+		} 	
+
+    }
 	
 })
